11ee44998ef40767d5106434adad9c8e170c2ad7
Joseph Piperakis <i.piperakis@gmail.com>
2019-01-15 02:02:36 +0100

    Initial commit. Basic PID implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,5 +1,6 @@
 #include "PID.h"
-
+#include <iostream>
+using namespace std;
 /**
  * TODO: Complete the PID class. You may add any additional desired functions.
  */
@@ -12,6 +13,13 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
 
 }
 
@@ -19,12 +27,17 @@ void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-
+  p_error = cte;
+  i_error +=cte;
+  d_error = cte-p_error;
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  float total_err;
+  cout << "p_error" <<Kp*p_error << endl;
+  total_err = -Kp*p_error - Ki*i_error - Kd*d_error;
+  return total_err;  // TODO: Add your total error calc here!
 }
